2060f01f39019f390627d675bd65728a74bb6a30
amir-raad <raad@mail.com>
2025-09-30 13:03:25 +0330

    init

---
+++ src/main/java/ir/ipaam/fileservice/FileserviceApplication.java
@@ -0,0 +1,11 @@
+package ir.ipaam.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileserviceApplication {
+public static void main(String[] args) {
+SpringApplication.run(FileserviceApplication.class, args);
+}
+}
